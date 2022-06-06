497937f18f843a30b0b90bc2d07ce7af11449aa1
Anthony COLAS <anthonycolas.france@gmail.com>
2022-06-06 18:40:40 +0200

    Created a second branch more visibility during explainations

--- scripts/models/dataManager.js
+++ scripts/models/dataManager.js
@@ -49,20 +49,15 @@ export default class dataManager {
 
     //Recherche via TITRE, INGREDIENTS, DESCRIPTION avec la searchBar
     static filterData(term) {
-        //  Recherche avec une fonction FOR LOOP
-        let newFilteredData = [];
-        for (let i = 0; i < this.filteredData.length; i++) {
-            if (
-                this.filteredData[i].name.toLowerCase().includes(term) ||
-                this.filteredData[i].description.toLowerCase().includes(term) ||
-                this.filteredData[i].ingredients.some((ingredient) =>
+        //  Recherche avec la fonction JavaScript Filter
+        this.filteredData = this.filteredData.filter(
+            (recipe) =>
+                recipe.name.toLowerCase().includes(term) ||
+                recipe.description.toLowerCase().includes(term) ||
+                recipe.ingredients.some((ingredient) =>
                     ingredient.ingredient.includes(term)
                 )
-            ) {
-                newFilteredData.push(this.filteredData[i]);
-            }
-        }
-        this.filteredData = [...newFilteredData];
+        );
     }
 
     //Recherche ingredient dans le sous-menu filtre avancé
